d610df2eead4184045d692b67dc56c5e77c34211
Justin <jribarich98@gmail.com>
2022-07-21 22:16:38 -0400

    Project FINISHED
    Had to switch up alot of their HTML since .keyCode is 'depracated'...

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  if (!audio) return; // Stop function from running
+  audio.currentTime = 0; // Rewind to start
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
